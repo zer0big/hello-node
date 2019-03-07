325885cbeeb491095aec877b3ca82e4479365cb5
tshaiman <tomer.shaiman@gmail.com>
2019-03-07 21:50:13 +0200

    config-maps

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const config = require('config')
 const app = express()
-const port = process.env.PORT || 8080
+const port =  config.server.port || 8080
+
+console.log('app data is ' + config.app["my-data"])
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port} !`))
